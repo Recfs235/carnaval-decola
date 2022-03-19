cc21a10800e631ff8a17091e1d67076fd5ded661
Regiane Santos <re.cfs235@gmail.com>
2022-03-19 12:14:29 -0300

    componente-bloco

---
+++ src/components/BlocosCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="blocos-card">
+    <ul>
+      <!-- <li v-for="(bloco, index) of getDataFetch" :key="bloco.name">
+        <p><strong>Adress: </strong>{{ bloco.adress }}</p>
+        <p><strong>Description: </strong>{{ bloco.description }}</p>
+        <p><strong>Nome do Evento: </strong>{{ bloco.name }}</p>
+        <img :src="bloco.photo" :alt="animal.nome">
+      </li> -->
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BlocosCard',
+  props: {
+    getDataFetch: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      // variaveis: '',
+
+    }
+  },
+  methods: {
+    // vazio(){}
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
